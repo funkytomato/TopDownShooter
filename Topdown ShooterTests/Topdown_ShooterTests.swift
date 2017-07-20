60892a5a2768f23a47273315cd8d21e7c0254d2c
Jason Fry <funkytomato@gmail.com>
2017-07-20 14:59:16 +0200

    Initial Commit

---
+++ Topdown ShooterTests/Topdown_ShooterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Topdown_ShooterTests.swift
+//  Topdown ShooterTests
+//
+//  Created by cadet on 20/07/2017.
+//  Copyright © 2017 Jason Fry. All rights reserved.
+//
+
+import XCTest
+@testable import Topdown_Shooter
+
+class Topdown_ShooterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
